bf9bfc7dd46546d9a976ae642ae0d93207be2bba
Mohammad Khan <mohammad.khan@gmx.us>
2018-01-11 11:09:16 -0800

    added initial version available_moves for Knight

--- lib/piece/knight.rb
+++ lib/piece/knight.rb
@@ -1,4 +1,5 @@
 class Knight < Piece
+  attr_reader :r_idx, :c_idx
 
   def initialize r_idx, c_idx
     @r_idx = r_idx
@@ -7,5 +8,39 @@ class Knight < Piece
 
   def name
     "K"
+  end
+
+  def available_moves
+    # 8 possible moves from the logic below
+    # - row increment/decrement by 1, col decrement/indrement by 2
+    # - col increment/decrement by 1, row decrement/indrement by 2
+
+    moves = []
+
+    if r_idx + 1 <= 7
+      moves << [r_idx + 1, c_idx + 2] if c_idx + 2 <= 7
+      moves << [r_idx + 1, c_idx - 2] if c_idx - 2 >= 0
+    end
+
+    if r_idx - 1 >= 0
+      moves << [r_idx - 1, c_idx + 2] if c_idx + 2 <= 7
+      moves << [r_idx - 1, c_idx - 2] if c_idx - 2 >= 0
+    end
+
+    if c_idx + 1 <= 7
+      moves << [r_idx + 2, c_idx + 1] if r_idx + 2 <= 7
+      moves << [r_idx - 2, c_idx + 1] if r_idx - 2 >= 0
+    end
+
+    if c_idx - 1 >= 0
+      moves << [r_idx + 2, c_idx - 1] if r_idx + 2 <= 7
+      moves << [r_idx - 2, c_idx - 1] if r_idx - 2 >= 0
+    end
+
+    # TODO:
+    # - Add test cases
+    # - similar code, may have room for meta-programming, possible refactor
+
+    moves
   end
 end
