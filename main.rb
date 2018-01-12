f6380a2f0c0073e49c30dbcc51c017a4efd02f17
Mohammad Khan <mohammad.khan@gmx.us>
2018-01-12 17:44:56 -0600

    updated main script to catch exception and terminate nicely

--- main.rb
+++ main.rb
@@ -10,17 +10,19 @@ end
 piece = ARGV.shift || help
 position = ARGV.shift || help
 
-# TODO:
-# - Validate the input
-# - pass the input the library to get available move
+begin
+  board = Board.init_with_pieces([[piece, position]])
+  moves = board.available_moves(position).collect { |coord|
+    position = Board.position(coord)
+    board.add_piece('pawn', position) # just for display purpose
+    position
+  }
 
-board = Board.init_with_pieces([[piece, position]])
-moves = board.available_moves(position).collect { |coord|
-  position = Board.position(coord)
-  board.add_piece('pawn', position) # just for display purpose
-  position
-}
+  puts moves.join(', ')
 
-puts moves.join(', ')
-
-board.display
+  board.display
+rescue PositionError => e
+  puts e.message
+rescue => e
+  puts e.message
+end
